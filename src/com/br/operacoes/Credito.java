f56237f00152853c8610147c905105a869b42d30
Silas <silastiagoo@gmail.com>
2015-03-30 21:46:13 -0300

    Crédito no valor XX para a conta YY realizado com sucesso #1

--- src/com/br/operacoes/Credito.java
+++ src/com/br/operacoes/Credito.java
@@ -13,7 +13,7 @@ import com.br.model.Conta;
  */
 
 public class Credito implements Command{
-
+	
 	
 	public Credito() {
 		super();
@@ -26,10 +26,13 @@ public class Credito implements Command{
 			HttpServletResponse response) {
 		// TODO Auto-generated method stub
 		Object cc = new Object();
+		Object valorCredito = new Object();
 		HttpSession session = request.getSession();
+		//valorCredito = request.getAttribute("valor");
 		cc = session.getAttribute("cc");
 		int valor = Integer.parseInt(request.getParameter("valor"));
 		int resultado = 0;
+		
 		
 		Conta conta = new Conta();
 		conta.setCc(cc.toString());
@@ -42,12 +45,13 @@ public class Credito implements Command{
 
 			conta.setValor(resultado);
 			contadao.Saque(conta);
-
+			response.setContentType("text/html");
+			session.setAttribute("valorCredito", valor);
 		}catch(ClassNotFoundException e){
 			e.printStackTrace();
 		}
 		
-		return "inicio.jsp";
+		return "credito.jsp";
 
 	}
 
