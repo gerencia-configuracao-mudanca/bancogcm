56a60ed72a4ad49d1b3ce4313bf3fcf5d6bd18f4
ronimour <ronimour@gmail.com>
2015-06-18 12:18:51 -0300

    correcao dos testes

---
+++ src/test/bancogcm/CreditoTest.java
@@ -0,0 +1,15 @@
+package test.bancogcm;
+
+import static org.junit.Assert.*;
+
+import org.junit.Test;
+
+
+public class CreditoTest {
+
+	@Test
+	public void testExecute() {
+		assertTrue(false);
+	}
+
+}
---
+++ src/test/bancogcm/LogoutTest.java
@@ -0,0 +1,15 @@
+package test.bancogcm;
+
+import static org.junit.Assert.*;
+
+import org.junit.Test;
+
+
+public class LogoutTest {
+
+	@Test
+	public void testExecute() {
+		assertTrue(false);
+	}
+
+}
